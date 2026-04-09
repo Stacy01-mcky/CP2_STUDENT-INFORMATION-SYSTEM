decaba0d9b1742c62e273637e963c74429800b6a
Stacy_16 <stacyfeoliveros@gmail.com>
2026-04-09 15:11:19 +0800

    Created array to store student records ,Implemented display menu function.py

---
+++ student_info_system.py
@@ -0,0 +1,17 @@
+# Student Information System
+# Requirements: Functions, Arrays, Looping Menu, Input Validation, Clean Code
+# OLIVEROS(INTRO - VIEW STUDENT) 
+# INSORIO ( SEARCH - END)
+# MY INTRO CODE BY DOING MYSELF
+
+students = []  # Array to store student records
+
+# Display menu
+def display_menu():
+    print("===== Student Information System =====")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Delete Student")
+    print("5. Exit")
+
