6a2766e9de91f0c9946d91a6a1c7672b64a7d2ca
Stacy_16 <stacyfeoliveros@gmail.com>
2026-05-15 17:43:16 +0800

    Update Full code.py

--- Full code.py
+++ Full code.py
@@ -2,7 +2,24 @@
 # Student Information System
 # Requirements: Functions, Arrays, Looping Menu, Input Validation, Clean Code
 
+import json
 students = []  # Array to store student records
+FILE_NAME = "students.json"
+
+# ---------- File Handling ----------
+def load_students():
+    """Load student records from file if available."""
+    global students
+    try:
+        with open(FILE_NAME, "r") as file:
+            students = json.load(file)
+    except FileNotFoundError:
+        students = []
+
+def save_students():
+    """Save student records to file."""
+    with open(FILE_NAME, "w") as file:
+        json.dump(students, file, indent=4)
 
 
 # Display menu
