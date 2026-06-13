85dc3c0e53b9de6f88809606ba2a7bdce5c2aad2
Stacy_16 <stacyfeoliveros@gmail.com>
2026-06-13 13:02:08 +0800

    to handle menu loop and imports only.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#Main Program loop 
+# ---------- Main Loop ----------
+from file_handler import load_students
+from operations import add_student, view_students, search_student, delete_student, update_student, show_statistics
 def main():
     load_students()
     while True:
@@ -14,12 +16,14 @@ def main():
         elif choice == "4":
             delete_student()
         elif choice == "5":
+            update_student()
+        elif choice == "6":
+            show_statistics()
+        elif choice == "7":
             print("Exiting program... THANK YOU!")
             break
         else:
             print("Invalid choice!")
 
-
-# Run the program
 if __name__ == "__main__":
     main()
